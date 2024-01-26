ab1ec28497cb1544f325edf24d3f8c5330fa913b
luoqiuyun <qiuyun.luo@outlook.com>
2024-01-26 12:32:16 -0800

    process window resize

--- src/components/Weekdays/index.tsx
+++ src/components/Weekdays/index.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import './styles.css';
 
 const Weekdays: React.FC = () => {
-  
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const isMobile = width <= 768;
+  const windowResize = () => setWidth(window.innerWidth);
+
   const daysOfWeek = [
     'Sunday',
     'Monday',
@@ -12,12 +15,17 @@ const Weekdays: React.FC = () => {
     'Friday',
     'Saturday'
   ];
-  
+
+  useEffect(() => {
+    window.addEventListener('resize', windowResize);
+    return () => window.removeEventListener('resize', windowResize);
+  }, []);
+
   return (
     <div className="weekday-container">
       {daysOfWeek.map((weekday, i) => 
         <div key={`weekday-${i}`} className="week-day">
-          {weekday.substr(0, 2)}
+          {!isMobile ? weekday : weekday.substr(0, 2)}
         </div>
       )}
     </div>
